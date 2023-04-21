b69328dcc77a5a3823069eb880058341a4a5bd69
Pilla <manojkumar.pilla@dxc.com>
2023-04-21 14:29:08 +0530

    first assingmet

---
+++ assingment/calender_1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Enter month(Press 1 for Jan, 2 for Feb, 12 for Dec......): ");
+int month = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter year: ");
+int year = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Day of week(Press 1 for Mon, 2 for Tue, 7 for Sun......): ");
+int startDay = Convert.ToInt32(Console.ReadLine());
+
+int numDays = DateTime.DaysInMonth(year, month);
+
+Console.WriteLine("      Calendar       ");
+Console.WriteLine(" S  M  Tu W Th  F  S");
+
+for (int i = 1; i <= startDay; i++)
+{
+    Console.Write("   ");
+}
+for (int day = 1; day <= numDays; day++)
+{
+    Console.Write($"{day,2} ");
+    if ((day + startDay) % 7 == 0 || day == numDays)
+    {
+        Console.WriteLine();
+    }
+}
